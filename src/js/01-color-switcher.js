e960b09d87985b6f0a3aa10edee0ddc198fd0007
Kate Panchenko <k.panchenko12345@gmail.com>
2022-12-26 17:27:23 +0200

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let setTimer = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn.addEventListener('click', randomColorOn);
+stopBtn.addEventListener('click', randomColorOff)
+
+function randomColorOn () {
+    setTimer = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    
+};
+
+function randomColorOff(params) {
+    setTimer = clearInterval(setTimer);
+    document.body.style.backgroundColor = "";
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
 
